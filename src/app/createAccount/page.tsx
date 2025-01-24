cdd305edff5e2b2c6fd50d82dd1797e88737eb7c
jojayama <jodiyamane27@gmail.com>
2025-01-24 14:54:30 -0800

    feat: redid design on the create account page

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { Box, FormControl, FormLabel, FormErrorMessage, Input, Button } from "@chakra-ui/react";
+import { Image, Box, FormControl, FormLabel, FormErrorMessage, Input, Button, Link } from "@chakra-ui/react";
 import { useState } from "react";
 import "../fonts/fonts.css";
 
-type FormFields = "name" | "username" | "password" | "phoneNumber" | "email";
+type FormFields = "name" | "username" | "password" | "confirmPass" | "email";
 
 export default function CreateAccount() {
   const [formState, setFormState] = useState<Record<FormFields, { value: string; touched: boolean }>>({
     name: { value: "", touched: false },
     username: { value: "", touched: false },
     password: { value: "", touched: false },
-    phoneNumber: { value: "", touched: false },
+    confirmPass: { value: "", touched: false },
     email: { value: "", touched: false },
   });
 
@@ -34,7 +34,7 @@ export default function CreateAccount() {
     <div
       style={{
         position: "absolute",
-        backgroundColor: "#eeeeee",
+        backgroundColor: "#596334",
         height: "100vh",
         width: "100%",
       }}
@@ -43,26 +43,63 @@ export default function CreateAccount() {
         style={{
           backgroundColor: "white",
           justifySelf: "center",
-          width: "50%",
+          width: "500px",
           marginTop: "10vh",
           borderRadius: "41px",
           fontFamily: "Inter",
-          paddingBottom: "20px",
+          paddingBottom: "40px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
         }}
       >
-        <h1 style={{ width: "100%", textAlign: "center", fontSize: "40px" }}>Create Account</h1>
+        <Box
+          style={{
+            position: "absolute",
+            top: "10px", // Half the height of the circular box
+            left: "50%",
+            transform: "translateX(-50%)",
+            borderRadius: "1000px",
+            backgroundColor: "#F4F1E8",
+            width: "150px",
+            height: "150px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src="/logo1.png"
+            alt="Logo"
+            style={{
+              width: "70%",
+              height: "70%",
+              objectFit: "contain",
+            }}
+          />
+        </Box>
+        <p style={{ color: "#596334", textAlign: "center", marginTop: "25px", paddingTop: "70px" }}>
+          Sign in to continue to your <br /> CCHTF dashboard
+        </p>
         <FormControl
           isInvalid={isError("name")}
           style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
         >
-          <FormLabel style={{ width: "80%", textAlign: "left", marginTop: "10px" }}>Name</FormLabel>
           <Input
             type="text"
             value={formState.name.value}
             onChange={handleInputChange("name")}
             onBlur={handleBlur("name")}
             placeholder="Name"
-            style={{ width: "80%" }}
+            _placeholder={{ color: "inherit" }}
+            style={{
+              width: "60%",
+              height: "50px",
+              border: "none",
+              backgroundColor: "#F4F1E8",
+              color: "#33333",
+              borderRadius: "16px",
+            }}
           />
           {isError("name") && <FormErrorMessage>Field is required.</FormErrorMessage>}
         </FormControl>
@@ -71,14 +108,21 @@ export default function CreateAccount() {
           isInvalid={isError("username")}
           style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
         >
-          <FormLabel style={{ width: "80%", textAlign: "left", marginTop: "10px" }}>Username</FormLabel>
           <Input
             type="text"
             value={formState.username.value}
             onChange={handleInputChange("username")}
             onBlur={handleBlur("username")}
             placeholder="Username"
-            style={{ width: "80%" }}
+            _placeholder={{ color: "inherit" }}
+            style={{
+              width: "60%",
+              height: "50px",
+              border: "none",
+              backgroundColor: "#F4F1E8",
+              color: "#33333",
+              borderRadius: "16px",
+            }}
           />
           {isError("username") && <FormErrorMessage>Field is required.</FormErrorMessage>}
         </FormControl>
@@ -87,52 +131,90 @@ export default function CreateAccount() {
           isInvalid={isError("password")}
           style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
         >
-          <FormLabel style={{ width: "80%", textAlign: "left", marginTop: "10px" }}>Password</FormLabel>
           <Input
             type="password"
             value={formState.password.value}
             onChange={handleInputChange("password")}
             onBlur={handleBlur("password")}
             placeholder="Password"
-            style={{ width: "80%" }}
+            _placeholder={{ color: "inherit" }}
+            style={{
+              width: "60%",
+              height: "50px",
+              border: "none",
+              backgroundColor: "#F4F1E8",
+              color: "#33333",
+              borderRadius: "16px",
+            }}
           />
           {isError("password") && <FormErrorMessage>Field is required.</FormErrorMessage>}
         </FormControl>
 
         <FormControl
-          isInvalid={isError("phoneNumber")}
+          isInvalid={isError("confirmPass")}
           style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
         >
-          <FormLabel style={{ width: "80%", textAlign: "left", marginTop: "10px" }}>Phone Number</FormLabel>
           <Input
             type="tel"
-            value={formState.phoneNumber.value}
-            onChange={handleInputChange("phoneNumber")}
-            onBlur={handleBlur("phoneNumber")}
-            placeholder="+1 "
-            style={{ width: "80%" }}
+            value={formState.confirmPass.value}
+            onChange={handleInputChange("confirmPass")}
+            onBlur={handleBlur("confirmPass")}
+            placeholder="Confirm Password "
+            _placeholder={{ color: "inherit" }}
+            style={{
+              width: "60%",
+              height: "50px",
+              border: "none",
+              backgroundColor: "#F4F1E8",
+              color: "#33333",
+              borderRadius: "16px",
+            }}
           />
-          {isError("phoneNumber") && <FormErrorMessage>Field is required.</FormErrorMessage>}
+          {isError("confirmPass") && <FormErrorMessage>Field is required.</FormErrorMessage>}
         </FormControl>
 
         <FormControl
           isInvalid={isError("email")}
           style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
         >
-          <FormLabel style={{ width: "80%", textAlign: "left", marginTop: "10px" }}>Email Address</FormLabel>
           <Input
             type="email"
             value={formState.email.value}
             onChange={handleInputChange("email")}
             onBlur={handleBlur("email")}
             placeholder="Email"
-            style={{ width: "80%" }}
+            _placeholder={{ color: "inherit" }}
+            style={{
+              width: "60%",
+              height: "50px",
+              border: "none",
+              backgroundColor: "#F4F1E8",
+              color: "#33333",
+              borderRadius: "16px",
+            }}
           />
           {isError("email") && <FormErrorMessage>Field is required.</FormErrorMessage>}
         </FormControl>
         <Box style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
-          <Button>Submit</Button>
+          <Button
+            style={{
+              backgroundColor: "#596334",
+              color: "white",
+              width: "60%",
+              fontWeight: "lighter",
+              height: "50px",
+              borderRadius: "16px",
+            }}
+          >
+            Sign Up
+          </Button>
         </Box>
+        <p style={{ textAlign: "center", color: "#596334" }}>
+          Already have an account?{" "}
+          <Link href="/login" style={{ textDecoration: "underline" }}>
+            Sign in
+          </Link>
+        </p>
       </Box>
     </div>
   );
