2c4cad92fc8f0ae0b78e1ce318fe1c41888e5011
Brandon <ebdon101@gmail.com>
2025-01-24 14:57:09 -0800

    create tree schema

--- src/database/treeSchema.ts
+++ src/database/treeSchema.ts
@@ -1,5 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
-const TreeSchema = new Schema({});
+const TreeSchema = new Schema({
+  collectorName: { type: String, required: true },
+  dateCollected: { type: String, required: true },
+  gpsCoordinates: { type: String, required: true },
+  photo: { type: String, required: false },
+  dbh: { type: mongoose.Types.Decimal128, required: true },
+  canopyBreadth: { type: mongoose.Types.Decimal128, required: true },
+  species: { type: String, required: true },
+  treeCondition: { type: [String], required: true },
+  additionalNotes: {
+    type: String,
+    required: false,
+  },
+});
 
 export default mongoose.model("Tree", TreeSchema);
