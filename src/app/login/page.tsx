7572fff7f8bfe2983bec80e9a4faab0d2a2666fb
soramicha <schang7jcu@gmail.com>
2025-01-25 19:41:40 -0800

    Updated Login Page with Finalized Designs

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Center, Text, Box, Button, Input, FormControl, FormLabel } from "@chakra-ui/react";
+import { Center, VStack, Image, Text, Link, Box, Button, Input, FormControl, Circle } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 export default function Login() {
@@ -15,34 +15,62 @@ export default function Login() {
   }
 
   return (
-    <div>
+    <div style={{ backgroundColor: "#596334" }}>
       {/* put login form into a box */}
       <Center width={"100vw"} height={"100vh"}>
+        {/* logo */}
+        <Circle mb={325} position="absolute" size="150px" backgroundColor="#F4F1E8"></Circle>
+        <Image
+          mb={325}
+          position="absolute"
+          boxSize="120px"
+          borderRadius="full"
+          fit="contain"
+          alt="Logo not Appearing"
+          src="/logo.png"
+        ></Image>
         <Box
+          mt={20}
           alignItems="center"
-          borderRadius="md"
+          borderRadius="xl"
           borderColor="black"
-          borderWidth={1}
           bg="white"
-          w="500px"
+          w="400px"
           p={10}
           color="black"
         >
-          <Text mb={4} align="center" fontSize="30px">
-            Sign In
+          <Text color="#596334" align="center" mt={35}>
+            Sign in to continue to your
           </Text>
-          <Text color="gray" align="center" mb={4}>
-            Sign in to continue to your CCHTF dashboard
+          <Text color="#596334" align="center">
+            CCHTF dashboard{" "}
           </Text>
           <FormControl>
-            <FormLabel>Email</FormLabel>
             {/* update value of email and password every time user types */}
-            <Input value={email} onChange={(e) => setEmail(e.target.value)} type="email"></Input>
-            <FormLabel>Password</FormLabel>
-            <Input value={password} onChange={(e) => setPassword(e.target.value)} type="text"></Input>
-            <Button color="white" bg="gray" w="100%" onClick={signIn} mt={4} type="submit">
+            <Input
+              backgroundColor="#F4F1E8"
+              mt={4}
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+            ></Input>
+            <Input
+              backgroundColor="#F4F1E8"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="text"
+              mt={4}
+            ></Input>
+            <Button color="white" bg="#596334" w="100%" onClick={signIn} mt={4} type="submit">
               Sign in
             </Button>
+            <Center>
+              <Text color="#596334" mt={4}>
+                Don&#39;t have an account?<Link href="/"> Sign up</Link>
+              </Text>
+            </Center>
           </FormControl>
         </Box>
       </Center>
