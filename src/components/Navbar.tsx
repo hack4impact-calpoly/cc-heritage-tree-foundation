1af7b77100f47fc620555ccd592758fe80b5132e
Sam Phan <nsphan1207@gmail.com>
2025-01-28 14:38:39 -0800

    refactor: Dynamically create navbar items using array

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,28 @@
-import style from "@/components/Navbar.module.css";
-import { Box, VStack, Image, Button } from "@chakra-ui/react";
+import { Box, VStack, Image, Button, Icon } from "@chakra-ui/react";
 import "@/app/fonts/fonts.css";
 import { LuTrees } from "react-icons/lu";
 import { MdOutlineDashboard, MdOutlinePeopleAlt } from "react-icons/md";
+import { IconType } from "react-icons";
 
+interface NavItem {
+  text: string;
+  icon: IconType;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    text: "Dashboard",
+    icon: MdOutlineDashboard,
+  },
+  {
+    text: "Tree Inventory",
+    icon: LuTrees,
+  },
+  {
+    text: "Volunteers",
+    icon: MdOutlinePeopleAlt,
+  },
+];
 
 export default function Navbar() {
   return (
@@ -14,33 +33,27 @@ export default function Navbar() {
         width: "10%",
       }}
     >
-      {/*Box for NavBar*/}
+      {/*Box for Logo*/}
       <Box>
-        {/*Box for Logo*/}
-        <Box>
-          <Image
-            src="/logo1.png"
-            alt="Logo"
-            style={{
-              width: "70%",
-              height: "70%",
-              objectFit: "contain",
-            }}
-          />
-        </Box>
-        {/*VStack for Items*/}
-        <VStack>
-          <Button>
-            <MdOutlineDashboard /> Dashboard
-          </Button>
-          <Button>
-            <LuTrees /> Tree Inventory
-          </Button>
-          <Button>
-            <MdOutlinePeopleAlt /> Volunteers
-          </Button>
-        </VStack>
+        <Image
+          src="/logo1.png"
+          alt="Logo"
+          style={{
+            width: "70%",
+            height: "70%",
+            objectFit: "contain",
+          }}
+        />
       </Box>
+      {/*VStack for Items*/}
+      <VStack>
+        {NAV_ITEMS.map((NavItem) => (
+          <Button key={NavItem.text}>
+            {<NavItem.icon />}
+            {NavItem.text}
+          </Button>
+        ))}
+      </VStack>
       <div>NavBar</div>
     </div>
   );
