702ca2a0a8ebeca49bbeec832bc87f1d6b292e5e
Sam Phan <nsphan1207@gmail.com>
2025-01-28 13:23:33 -0800

    feat: Add buttons and react-icons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,47 @@
+import style from "@/components/Navbar.module.css";
+import { Box, VStack, Image, Button } from "@chakra-ui/react";
+import "@/app/fonts/fonts.css";
+import { LuTrees } from "react-icons/lu";
+import { MdOutlineDashboard, MdOutlinePeopleAlt } from "react-icons/md";
+
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        height: "100vh",
+        width: "10%",
+      }}
+    >
+      {/*Box for NavBar*/}
+      <Box>
+        {/*Box for Logo*/}
+        <Box>
+          <Image
+            src="/logo1.png"
+            alt="Logo"
+            style={{
+              width: "70%",
+              height: "70%",
+              objectFit: "contain",
+            }}
+          />
+        </Box>
+        {/*VStack for Items*/}
+        <VStack>
+          <Button>
+            <MdOutlineDashboard /> Dashboard
+          </Button>
+          <Button>
+            <LuTrees /> Tree Inventory
+          </Button>
+          <Button>
+            <MdOutlinePeopleAlt /> Volunteers
+          </Button>
+        </VStack>
+      </Box>
+      <div>NavBar</div>
+    </div>
+  );
 }
