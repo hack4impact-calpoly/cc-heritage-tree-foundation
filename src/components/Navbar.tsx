f14a79b355bbf978023ea71775244f51cd102039
Sam Phan <nsphan1207@gmail.com>
2025-01-28 20:29:28 -0800

    feat: Align logo and button icons and texts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, VStack, Image, Button, Icon } from "@chakra-ui/react";
+import { Box, VStack, Image, Button, Flex } from "@chakra-ui/react";
 import "@/app/fonts/fonts.css";
 import { LuTrees } from "react-icons/lu";
 import { MdOutlineDashboard, MdOutlinePeopleAlt } from "react-icons/md";
@@ -7,6 +7,7 @@ import { IconType } from "react-icons";
 const COLORS = {
   primary: "#596435",
   secondary: "#e0ee99",
+  white: "white",
 };
 
 interface NavItem {
@@ -35,39 +36,59 @@ export default function Navbar() {
       style={{
         position: "sticky",
         height: "100vh",
-        width: "17rem",
+        width: "15rem",
         backgroundColor: COLORS.primary,
+        borderTopRightRadius: "20px",
+        borderBottomRightRadius: "20px",
+        justifyContent: "center",
       }}
     >
-      {/*Box for Logo*/}
-      <Box>
-        <Image
-          src="/logo1.png"
-          alt="Logo"
-          style={{
-            width: "70%",
-            height: "70%",
-            objectFit: "contain",
-          }}
-        />
-      </Box>
-      {/*VStack for Items*/}
       <VStack>
-        {NAV_ITEMS.map((NavItem) => (
-          <Button
-            key={NavItem.text}
+        <Box
+          style={{
+            backgroundColor: "white",
+            borderRadius: "100%",
+            height: "5rem",
+            width: "5rem",
+            alignContent: "center",
+          }}
+        >
+          <Image
+            src="/logo1.png"
+            alt="Logo"
             style={{
-              backgroundColor: COLORS.secondary,
-              color: COLORS.primary,
-              borderRadius: "20px",
-              width: "80%",
+              width: "70%",
+              height: "70%",
+              objectFit: "contain",
+              justifySelf: "center",
             }}
-          >
-            {<NavItem.icon />}
-            {NavItem.text}
-          </Button>
-        ))}
+          />
+        </Box>
+        {/*VStack for Items*/}
+        <VStack>
+          {NAV_ITEMS.map((NavItem) => (
+            <Button
+              key={NavItem.text}
+              style={{
+                backgroundColor: COLORS.secondary,
+                color: COLORS.primary,
+                borderRadius: "20px",
+                width: "100%",
+                height: "2rem",
+                justifyContent: "left",
+              }}
+            >
+              <NavItem.icon
+                style={{
+                  marginRight: "1rem",
+                }}
+              />
+              {NavItem.text}
+            </Button>
+          ))}
+        </VStack>
       </VStack>
+      {/*Box for Logo*/}
       <div>NavBar</div>
     </div>
   );
