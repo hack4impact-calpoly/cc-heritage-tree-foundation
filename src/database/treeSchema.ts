48e39615b43e740218230527142f35d2aa4b3b2e
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2025-01-31 07:01:35 +0800

    edit tree schema type

--- src/database/treeSchema.ts
+++ src/database/treeSchema.ts
@@ -3,20 +3,20 @@ import mongoose, { Decimal128, Schema } from "mongoose";
 export type ITree = {
   _id: string;
   collectorName: string;
-  dateCollected: string;
-  gpsCoordinates: string;
-  photo: string;
+  dateCollected: Date;
+  gpsCoordinates: Decimal128[];
+  photo?: string;
   dbh: Decimal128;
   canopyBreadth: Decimal128;
   species: string;
   treeCondition: string[];
-  additionalNotes: string;
+  additionalNotes?: string;
 };
 
 const TreeSchema = new Schema<ITree>({
   collectorName: { type: String, required: true },
-  dateCollected: { type: String, required: true },
-  gpsCoordinates: { type: String, required: true },
+  dateCollected: { type: Date, required: true },
+  gpsCoordinates: { type: [mongoose.Types.Decimal128], required: true },
   photo: { type: String, required: false },
   dbh: { type: mongoose.Types.Decimal128, required: true },
   canopyBreadth: { type: mongoose.Types.Decimal128, required: true },
