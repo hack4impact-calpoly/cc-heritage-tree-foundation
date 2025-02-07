699441691e994b4f4df2fc15b97a84d293063ab7
Sam Phan <nsphan1207@gmail.com>
2025-02-07 14:51:30 -0800

    feat: Touch tree-form-data.ts for separation of data models and frontend

---
+++ src/app/newTreeForm/tree-form-data.ts
@@ -0,0 +1,27 @@
+const COLORS = {};
+
+//Tree Types as on Figma
+export const treeTypes = ["Valley Oak", "Coast Live Oak", "Blue Oak"] as const;
+export type TreeType = (typeof treeTypes)[number];
+
+//Tree Issues as on Figma
+export const treeIssues = [
+  "Dead branches",
+  "Cavities or hollows",
+  "Ivy strangulation",
+  "Concrete strangulation",
+  "Soil compaction",
+  "Blunt trauma",
+  "Dual main competing trunk",
+  "Epicormic growth",
+] as const;
+export type treeIssue = (typeof treeIssues)[number];
+
+//Form Data
+export type TreeFormData = {
+  treeType: TreeType;
+  treeSpecs: Map<string, string>;
+  treeHealth: number; //Number between 1-10
+  treeIssues: Array<treeIssue>;
+  fieldNotes: string;
+};
