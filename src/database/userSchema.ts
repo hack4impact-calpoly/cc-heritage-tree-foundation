6f8b7bef5508446150db94d6b462cfcda2fc9e6e
Mio <manakaga@calpoly.edu>
2025-02-14 08:32:25 -0800

    feat: added user endpoints

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -2,8 +2,11 @@ import mongoose, { Schema } from "mongoose";
 
 //! Example user schema. Not guaranteed to work
 const UserSchema = new Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  role: { type: String, required: true },
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
