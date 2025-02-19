d21aadce0a986eadee67f9eca3351f473a545202
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2025-02-19 14:18:24 +0800

    rename volunteer to user

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,9 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
-//! Example user schema. Not guaranteed to work
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+export type IUser = {
+  _id: string;
+  name: string;
+  roles: string[];
+  email: string;
+  phoneNumber: string;
+};
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  roles: { type: [String], required: true },
+  email: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
 });
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
