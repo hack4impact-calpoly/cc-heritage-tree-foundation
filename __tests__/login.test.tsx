12bc3533f961720cc12e03ff023f773560caae0f
Emi <emidinh6@gmail.com>
2025-02-22 09:12:59 -0800

    added test file

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -4,40 +4,16 @@
 
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Login from "@/app/login/page";
-import userEvent from "@testing-library/user-event";
+import { usePathname } from "next/navigation";
 
-jest.mock("@clerk/nextjs", () => ({
-  //mocks the clerk provider, takes the children and outputs the children
-  ClerkProvider: ({ children }) => <>{children}</>,
-
-  //mocks the login component, it is just two divs
-  //two input boxes. This should catch if sign in component is forgotten
-  SignIn: () => (
-    <div>
-      <div>Sign in to CCHTF</div>
-      <input placeholder="Email" />
-      <input placeholder="Password" type="password" />
-    </div>
-  ),
-  //mocks the signed out component
-  SignedOut: ({ children }) => <>{children}</>,
+// Mock next/navigation
+jest.mock("next/navigation", () => ({
+  usePathname: jest.fn(() => "/"),
 }));
 
 describe("Login Page", () => {
-  it("allows email and password input", async () => {
-    render(<Login />);
-    const emailInput = screen.getByPlaceholderText("Email");
-    const passwordInput = screen.getByPlaceholderText("Password");
-
-    await userEvent.type(emailInput, "test@example.com");
-    await userEvent.type(passwordInput, "password123");
-
-    expect(emailInput).toHaveValue("test@example.com");
-    expect(passwordInput).toHaveValue("password123");
-  });
-
   it("renders the welcome message", () => {
     render(<Login />);
     expect(screen.getByText("Sign in to CCHTF")).toBeInTheDocument();
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -7,11 +7,26 @@ export default function ProfileCard() {
   const { user } = useUser();
 
   return (
-    <Flex justify="flex-end">
-      <SignedIn>
-        <UserButton />
-        <div>{user?.firstName}</div>
-      </SignedIn>
+    <Flex justify="flex-end" gap="4" bg="#F4F1E8">
+      <div>
+        <Box borderWidth="3px" padding={10} rounded="10" m="20px" bg="white">
+          <SignedIn>
+            <Flex align="center" gap={10}>
+              <UserButton></UserButton>
+              {/* <Image src={user?.imageUrl} alt="User Profile" boxSize="32px" borderRadius="50%" /> */}
+              <Flex direction="column">
+                <div>
+                  {user?.firstName} {user?.lastName}
+                </div>
+                <div style={{ color: "gray" }}>Role</div>
+              </Flex>
+              <Flex gap="4">
+                <Image src="/downArrow.svg" alt="Down Arrow" boxSize="24px" />
+              </Flex>
+            </Flex>
+          </SignedIn>
+        </Box>
+      </div>
     </Flex>
   );
 }
