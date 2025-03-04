fd10154c0f1085b091a1986237fccf36a9b89ce2
Emi <emidinh6@gmail.com>
2025-03-04 13:32:05 -0800

    feat: started off the messages page.

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,8 +1,30 @@
 "use client";
 import React, { useState } from "react";
+import { Flex, Box, Image, Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 
 function Messages() {
-  return <div>Messages</div>;
+  return (
+    <Box>
+      <div>Messages</div>
+      <div>10 Unread Messages</div>
+      <Tabs variant="soft-rounded" colorScheme="green">
+        <TabList>
+          <Tab>Inbox</Tab>
+          <Tab>Sent</Tab>
+        </TabList>
+        <TabPanels>
+          {/* This is panel is for Inbox Messages */}
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          {/* This panel is for Sent Messages */}
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
 }
 
 export default Messages;
