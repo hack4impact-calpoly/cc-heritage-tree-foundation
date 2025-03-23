45fbdb471f027b3b4e4030c0a472ca4ac4bcc320
Sophia Chang <schang7jcu@gmail.com>
2025-03-22 19:31:04 -0700

    completed messages mobile view

--- src/app/createAnnouncement/page.tsx
+++ src/app/createAnnouncement/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Button,
@@ -14,11 +13,18 @@ import {
   HStack,
   Text,
 } from "@chakra-ui/react";
+import { AlignJustify } from "lucide-react";
+import { BrowserView, MobileView } from "react-device-detect";
 import { AttachmentIcon } from "@chakra-ui/icons";
 import { InputStyleAnnouncement } from "@/styles/CreateAnnouncementStyle";
 
 const CreateAnnouncement = () => {
-  const { user } = useUser();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   type AnnouncementData = {
     recipients: string;
     subject: string;
@@ -50,124 +56,207 @@ const CreateAnnouncement = () => {
     }
   };
 
-  const handleSubmit = async () => {
-    try {
-      const from = user?.fullName || "Unknown Sender";
-      const to = formData.recipients.split(",").map((r) => r.trim());
-
-      const payload = {
-        from,
-        to,
-        subject: formData.subject,
-        message: formData.message,
-      };
-
-      console.log("Submitted Data: ", formData);
-
-      const res = await fetch("/api/messages", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      });
-
-      if (!res.ok) throw new Error("Failed to send announcement");
-
-      const data = await res.json();
-      console.log("Announcement sent:", data);
-    } catch (err) {
-      console.error("Error submitting:", err);
-    }
+  const handleSubmit = () => {
+    console.log("Submitted Data: ", formData);
   };
 
   return (
-    <Box
-      position="absolute"
-      width="100vw"
-      minHeight="100vh"
-      bg="#F4F1E8"
-      transform="translateX(-15rem)"
-      pl="15rem"
-      display="flex"
-      alignItems="center"
-    >
-      <VStack spacing={7} align="start" p="50px" py="50px" width="100%" maxW="900px" mx="auto">
-        <Box fontSize="3xl" fontWeight="bold">
-          New Message
-        </Box>
-        <FormControl>
-          <FormLabel fontWeight="bold">Send to</FormLabel>
-          <Input
-            name="recipients"
-            placeholder="Find recipients"
-            _placeholder={{ color: "#596435" }}
-            value={formData.recipients}
-            onChange={handleChange}
-            {...InputStyleAnnouncement}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel fontWeight="bold">Subject</FormLabel>
-          <Input
-            name="subject"
-            placeholder="Subject line here"
-            _placeholder={{ color: "#596435" }}
-            value={formData.subject}
-            onChange={handleChange}
-            {...InputStyleAnnouncement}
-          />
-        </FormControl>
-        <FormControl position="relative">
-          <FormLabel fontWeight="bold">Message</FormLabel>
-          <Textarea
-            name="message"
-            placeholder="Type your message here..."
-            _placeholder={{ color: "#596435" }}
-            value={formData.message}
-            onChange={handleChange}
-            {...InputStyleAnnouncement}
-            h="250px"
-            pr="40px" // Space for icon
-          />
-          <Box position="absolute" top="35px" right="15px" display="flex" alignItems="center">
-            <label htmlFor="file-upload" style={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
-              <IconButton
-                icon={<AttachmentIcon />}
-                bg="transparent"
-                color="black"
-                aria-label="Upload file"
-                _hover={{ color: "black" }}
-                as="label"
-                htmlFor="file-upload"
-                mr="-6px"
-              />
-              <Text fontSize="sm" color="black">
-                {formData.attachment ? formData.attachment.name : "Add attachment"}
-              </Text>
-            </label>
-          </Box>
-          <Input type="file" id="file-upload" display="none" onChange={handleFileChange} />
-        </FormControl>
-        <HStack spacing={5}>
-          <Button px="30px" fontWeight="normal" rounded="full" bg="#596435" color="white" onClick={handleSubmit}>
-            Send
-          </Button>
-          <Button
-            borderWidth="1.5px"
-            fontWeight="normal"
-            rounded="full"
-            variant="outline"
-            bg="white"
-            borderColor="#596435"
-            color="#596435"
-          >
-            Cancel
-          </Button>
-        </HStack>
-      </VStack>
-    </Box>
-    // </Box>
+    <div>
+      {isClient ? (
+        <>
+          <BrowserView>
+            <Box
+              position="absolute"
+              width="100vw"
+              minHeight="100vh"
+              bg="#F4F1E8"
+              transform="translateX(-15rem)"
+              pl="15rem"
+              display="flex"
+              alignItems="center"
+            >
+              <VStack spacing={7} align="start" p="50px" py="50px" width="100%" maxW="900px" mx="auto">
+                <Box fontSize="3xl" fontWeight="bold">
+                  New Message
+                </Box>
+                <FormControl>
+                  <FormLabel fontWeight="bold">Send to</FormLabel>
+                  <Input
+                    name="recipients"
+                    placeholder="Find recipients"
+                    _placeholder={{ color: "#596435" }}
+                    value={formData.recipients}
+                    onChange={handleChange}
+                    {...InputStyleAnnouncement}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontWeight="bold">Subject</FormLabel>
+                  <Input
+                    name="subject"
+                    placeholder="Subject line here"
+                    _placeholder={{ color: "#596435" }}
+                    value={formData.subject}
+                    onChange={handleChange}
+                    {...InputStyleAnnouncement}
+                  />
+                </FormControl>
+                <FormControl position="relative">
+                  <FormLabel fontWeight="bold">Message</FormLabel>
+                  <Textarea
+                    name="message"
+                    placeholder="Type your message here..."
+                    _placeholder={{ color: "#596435" }}
+                    value={formData.message}
+                    onChange={handleChange}
+                    {...InputStyleAnnouncement}
+                    h="250px"
+                    pr="40px" // Space for icon
+                  />
+                  <Box position="absolute" top="35px" right="15px" display="flex" alignItems="center">
+                    <label htmlFor="file-upload" style={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+                      <IconButton
+                        icon={<AttachmentIcon />}
+                        bg="transparent"
+                        color="black"
+                        aria-label="Upload file"
+                        _hover={{ color: "black" }}
+                        as="label"
+                        htmlFor="file-upload"
+                        mr="-6px"
+                      />
+                      <Text fontSize="sm" color="black">
+                        {formData.attachment ? formData.attachment.name : "Add attachment"}
+                      </Text>
+                    </label>
+                  </Box>
+                  <Input type="file" id="file-upload" display="none" onChange={handleFileChange} />
+                </FormControl>
+                <HStack spacing={5}>
+                  <Button
+                    px="30px"
+                    fontWeight="normal"
+                    rounded="full"
+                    bg="#596435"
+                    color="white"
+                    onClick={handleSubmit}
+                  >
+                    Send
+                  </Button>
+                  <Button
+                    borderWidth="1.5px"
+                    fontWeight="normal"
+                    rounded="full"
+                    variant="outline"
+                    bg="white"
+                    borderColor="#596435"
+                    color="#596435"
+                  >
+                    Cancel
+                  </Button>
+                </HStack>
+              </VStack>
+            </Box>
+          </BrowserView>
+          <MobileView>
+            <Box
+              position="absolute"
+              width="100vw"
+              minHeight="100vh"
+              bg="#F4F1E8"
+              transform="translateX(-15rem)"
+              pl="15rem"
+              display="flex"
+              alignItems="center"
+            >
+              <VStack spacing={7} align="start" p="50px" py="50px" width="100%" maxW="900px" mx="auto">
+                <Box fontSize="3xl" fontWeight="bold">
+                  New Message
+                </Box>
+                <FormControl>
+                  <FormLabel fontWeight="bold">Send to</FormLabel>
+                  <Input
+                    name="recipients"
+                    placeholder="Find recipients"
+                    _placeholder={{ color: "#596435" }}
+                    value={formData.recipients}
+                    onChange={handleChange}
+                    {...InputStyleAnnouncement}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontWeight="bold">Subject</FormLabel>
+                  <Input
+                    name="subject"
+                    placeholder="Subject line here"
+                    _placeholder={{ color: "#596435" }}
+                    value={formData.subject}
+                    onChange={handleChange}
+                    {...InputStyleAnnouncement}
+                  />
+                </FormControl>
+                <FormControl position="relative">
+                  <FormLabel fontWeight="bold">Message</FormLabel>
+                  <Textarea
+                    name="message"
+                    placeholder="Type your message here..."
+                    _placeholder={{ color: "#596435" }}
+                    value={formData.message}
+                    onChange={handleChange}
+                    {...InputStyleAnnouncement}
+                    h="250px"
+                    pr="40px" // Space for icon
+                  />
+                  <Box position="absolute" top="35px" right="15px" display="flex" alignItems="center">
+                    <label htmlFor="file-upload" style={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+                      <IconButton
+                        icon={<AttachmentIcon />}
+                        bg="transparent"
+                        color="black"
+                        aria-label="Upload file"
+                        _hover={{ color: "black" }}
+                        as="label"
+                        htmlFor="file-upload"
+                        mr="-6px"
+                      />
+                      <Text fontSize="sm" color="black">
+                        {formData.attachment ? formData.attachment.name : "Add attachment"}
+                      </Text>
+                    </label>
+                  </Box>
+                  <Input type="file" id="file-upload" display="none" onChange={handleFileChange} />
+                </FormControl>
+                <HStack spacing={5}>
+                  <Button
+                    px="30px"
+                    fontWeight="normal"
+                    rounded="full"
+                    bg="#596435"
+                    color="white"
+                    onClick={handleSubmit}
+                  >
+                    Send
+                  </Button>
+                  <Button
+                    borderWidth="1.5px"
+                    fontWeight="normal"
+                    rounded="full"
+                    variant="outline"
+                    bg="white"
+                    borderColor="#596435"
+                    color="#596435"
+                  >
+                    Cancel
+                  </Button>
+                </HStack>
+              </VStack>
+            </Box>
+          </MobileView>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,57 +4,41 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { ClerkProvider } from "@clerk/nextjs";
 import ProfileCard from "@/components/ProfileCard";
 import Navbar from "@/components/Navbar";
-import { useEffect, useState } from "react";
-import { isMobile } from "react-device-detect";
 
 import "./globals.css";
 import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathName = usePathname();
-  const [mounted, setMounted] = useState(false);
-  const [isMobileView, setIsMobileView] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-    setIsMobileView(isMobile);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     <ClerkProvider>
       <html lang="en">
         <body>
           <ChakraProvider>
-            {isMobileView ? (
-              <>{children}</>
-            ) : (
-              <div style={{ display: "flex", minHeight: "100vh" }}>
-                {pathName !== "/login" && pathName !== "/signup"}
-                <main
-                  style={{
-                    backgroundColor: "#F4F1E8",
-                    flexGrow: 1,
-                    paddingLeft:
-                      pathName !== "/volunteerDashboard" &&
-                      pathName !== "/volunteers" &&
-                      pathName !== "/treeTable" &&
-                      pathName !== "/newTreeForm"
-                        ? "15rem"
-                        : "0",
-                    width: "100%",
-                    height: "100%",
-                    minHeight: "100vh",
-                  }}
-                >
-                  <ProfileCard />
-                  {children}
-                </main>
-              </div>
-            )}
+            <div style={{ display: "flex", minHeight: "100vh" }}>
+              {pathName !== "/login" && pathName !== "/signup"}
+              <main
+                style={{
+                  backgroundColor: "#F4F1E8",
+                  flexGrow: 1,
+                  paddingLeft:
+                    pathName !== "/login" &&
+                    pathName !== "/signup" &&
+                    pathName !== "/messages" &&
+                    pathName !== "/adminDashboard" &&
+                    pathName !== "/createAnnouncement"
+                      ? "15rem"
+                      : "0",
+                  width: "100%",
+                  height: "100%",
+                  minHeight: "100vh",
+                }}
+              >
+                <ProfileCard />
+                {children}
+              </main>
+            </div>
           </ChakraProvider>
         </body>
       </html>
