30ff01dba1020fc20482e17c60c04edfcfaa5a94
Thomas Le <lethomas998@gmail.com>
2025-05-20 10:17:28 -0700

    completed issue

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -239,141 +239,152 @@ function Messages() {
           </BrowserView>
 
           <MobileView>
-            <div>
-              <div style={{ overflowX: "hidden" }}>
-                <div>
-                  <div>
-                    <div
-                      style={{
-                        position: "absolute",
-                        marginLeft: "20px",
-                        marginTop: "15px",
-                      }}
-                    >
-                      <AlignJustify></AlignJustify>
-                    </div>
-                    {/* add tree icon */}
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        marginTop: "50px",
-                      }}
-                    >
-                      <Image src="./logo1.png" alt="tree icon" height={"50px"}></Image>
-                    </div>
+            <Box padding="16px" overflowX="auto">
+              <Text className={styles.header} textAlign="center">
+                Messages
+              </Text>
+              <Text className={styles.unread} textAlign="center">
+                {unreadCount} unread {unreadCount === 1 ? "announcement" : "announcements"}
+              </Text>
 
-                    <button
-                      className={`${styles.tab} ${activeTab === "inbox" ? styles.activeTab : ""}`}
-                      style={{ marginTop: "20px", marginLeft: "10px" }}
-                      onClick={() => {
-                        setActiveTab("inbox");
-                        setCurrentPage(1);
-                      }}
-                    >
-                      Inbox
-                    </button>
-                    <button
-                      className={`${styles.tab} ${activeTab === "sent" ? styles.activeTab : ""}`}
-                      style={{ marginLeft: "10px" }}
-                      onClick={() => {
-                        setActiveTab("sent");
-                        setCurrentPage(1);
-                      }}
-                    >
-                      Sent
-                    </button>
-                  </div>
-                  {isAdmin && ( // Only show button if admin
-                    <button
-                      className={styles.newMessageButton}
-                      style={{
-                        position: "fixed",
-                        bottom: "20px",
-                        right: "20px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        zIndex: "9",
-                      }}
-                    >
-                      New Message +
-                    </button>
-                  )}
+              {/* Tabs */}
+              <Flex
+                className={styles.topBar}
+                justify="space-between"
+                align="center"
+                marginBottom="20px"
+                flexWrap="wrap"
+              >
+                <div className={styles.tabContainer}>
+                  <button
+                    className={`${styles.tab} ${activeTab === "inbox" ? styles.activeTab : ""}`}
+                    onClick={() => {
+                      setActiveTab("inbox");
+                      setCurrentPage(1);
+                    }}
+                  >
+                    Inbox
+                  </button>
+                  <button
+                    className={`${styles.tab} ${activeTab === "sent" ? styles.activeTab : ""}`}
+                    onClick={() => {
+                      setActiveTab("sent");
+                      setCurrentPage(1);
+                    }}
+                  >
+                    Sent
+                  </button>
                 </div>
-                {activeTab === "inbox" ? (
-                  <>
-                    <Stack backgroundColor={"white"} marginTop={7} borderRadius={10} padding={5} margin={3}>
-                      {currentMessages.map((msg) => (
-                        <div
-                          key={msg.id}
-                          className={msg.selected ? styles.fadedRow : ""}
-                          onClick={() => toggleSelect(msg.id)}
-                        >
-                          <div className={msg.selected ? styles.fadedText : ""}>
-                            <Flex alignItems={"center"}>
-                              <Avatar name={msg.sender} size="md" bg="#596334" color="white" />
-                              <div style={{ padding: "10px" }}>
-                                <Flex justify="space-between">
-                                  {msg.sender}
-                                  <Text className={msg.selected ? styles.fadedText : ""}>{msg.date}</Text>
-                                </Flex>
-                                <Text
-                                  style={{
-                                    overflow: "hidden",
-                                    whiteSpace: "nowrap",
-                                    textOverflow: "ellipsis",
-                                    maxWidth: "100%",
-                                  }}
-                                  className={msg.selected ? styles.fadedText : ""}
-                                >
-                                  {msg.message}
-                                </Text>
-                              </div>
-                            </Flex>
-                          </div>
-                          <div style={{ marginTop: "5px" }}>
-                            <Divider />
-                          </div>
-                        </div>
-                      ))}
-                    </Stack>
 
-                    {/* Pagination Controls */}
-                    <Box className={styles.pageControls} style={{ marginBottom: "70px" }}>
-                      <Button
-                        className={styles.pageButton}
-                        onClick={() => handlePageChange(currentPage - 1)}
-                        disabled={currentPage === 1}
-                      >
-                        Previous
-                      </Button>
-
-                      {Array.from({ length: totalPages }, (_, index) => (
-                        <Button
-                          key={index + 1}
-                          className={currentPage === index + 1 ? styles.activePage : styles.pageButton}
-                          onClick={() => handlePageChange(index + 1)}
-                        >
-                          {index + 1}
-                        </Button>
-                      ))}
-
-                      <Button
-                        className={styles.pageButton}
-                        onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage === totalPages}
-                      >
-                        Next
-                      </Button>
-                    </Box>
-                  </>
-                ) : (
-                  <p className={styles.sentMessage}>Sent messages here.</p>
+                {isAdmin && (
+                  <button className={styles.newMessageButton} onClick={() => router.push("/createAnnouncement")}>
+                    New Message +
+                  </button>
                 )}
-              </div>
-            </div>
+              </Flex>
+
+              {activeTab === "inbox" ? (
+                <Box overflowX="auto">
+                  <Table className={styles.table} size="sm">
+                    <Thead className={styles.tableHeader}>
+                      <Tr>
+                        <Th>Select</Th>
+                        <Th>Sender</Th>
+                        <Th>Subject Line</Th>
+                        <Th>Date</Th>
+                        <Th>Actions</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {currentMessages.map((msg) => (
+                        <Tr key={msg._id} className={styles.clickableRow}>
+                          <Td>
+                            <Checkbox isChecked={msg.selected} onChange={() => toggleSelect(msg.id)} />
+                          </Td>
+                          <Td
+                            className={`${msg.selected ? styles.fadedText : ""}`}
+                            onClick={() => setSelectedMessage(msg)}
+                          >
+                            <Flex className={styles.avatarContainer}>
+                              <Avatar name={msg.sender} size="sm" bg="#596334" color="white" />
+                              {msg.from}
+                            </Flex>
+                          </Td>
+                          <Td className={msg.selected ? styles.fadedText : ""}>{msg.subject}</Td>
+                          <Td className={msg.selected ? styles.fadedText : ""}>
+                            {new Date(msg.time).toLocaleDateString()}
+                          </Td>
+                          <Td>
+                            <ChevronRight
+                              onClick={() => {
+                                setOpenMessagePopUp(true);
+                                setMessageProps({
+                                  date: new Date(msg.time).toLocaleDateString(),
+                                  adminName: msg.from,
+                                  messageContent: msg.message,
+                                  messageTitle: msg.subject,
+                                  id: msg._id,
+                                });
+                              }}
+                            />
+                          </Td>
+                        </Tr>
+                      ))}
+                      {/* Fill blank rows if needed */}
+                      {Array.from({ length: 7 - currentMessages.length }).map((_, i) => (
+                        <Tr key={`empty-${i}`} style={{ height: "55px" }}>
+                          <Td colSpan={5}></Td>
+                        </Tr>
+                      ))}
+                    </Tbody>
+                    <Tfoot>
+                      <Tr>
+                        <Td colSpan={5}>
+                          <Box className={styles.pageControls}>
+                            <Button
+                              className={styles.pageButton}
+                              onClick={() => handlePageChange(currentPage - 1)}
+                              disabled={currentPage === 1}
+                            >
+                              Previous
+                            </Button>
+                            {Array.from({ length: totalPages }, (_, index) => (
+                              <Button
+                                key={index + 1}
+                                className={currentPage === index + 1 ? styles.activePage : styles.pageButton}
+                                onClick={() => handlePageChange(index + 1)}
+                              >
+                                {index + 1}
+                              </Button>
+                            ))}
+                            <Button
+                              className={styles.pageButton}
+                              onClick={() => handlePageChange(currentPage + 1)}
+                              disabled={currentPage === totalPages}
+                            >
+                              Next
+                            </Button>
+                          </Box>
+                        </Td>
+                      </Tr>
+                    </Tfoot>
+                  </Table>
+
+                  {/* Message popup */}
+                  {openMessagePopUp && (
+                    <MessagePopUp
+                      date={messageProps.date}
+                      messageTitle={messageProps.messageTitle}
+                      adminName={messageProps.adminName}
+                      messageContent={messageProps.messageContent}
+                      id={messageProps.id}
+                    />
+                  )}
+                </Box>
+              ) : (
+                <Text className={styles.sentMessage}>Sent messages here.</Text>
+              )}
+            </Box>
           </MobileView>
         </>
       ) : (
